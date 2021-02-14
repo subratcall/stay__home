37e58baf5b358cfa62faa557e8df6e0b26838b61
Dev-Beom <rtw_master@naver.com>
2021-02-14 18:49:29 +0900

    Update Model Structure

---
+++ stay__home/lib/model/http.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+
+class ModelGetAllUserInfo {}
+
+class ModelUpdateName {}
+
+class ModelUpdateTopTime {}
+
+class ModelUpdateHomeAddress {}
+
+class ModelInsertAccTime {}
+
+class ModelCreateAccount {}
