f535a6979a1f66ec941aa5d5ab8ef1ad38d57141
Dev-Beom <66074802+Dev-Beom@users.noreply.github.com>
2021-02-18 01:27:35 +0900

    Add Model"GetUserInfo"

--- stay__home/lib/model/http/ModelGetUserInfo.dart
+++ stay__home/lib/model/http/ModelGetUserInfo.dart
@@ -1,55 +1,56 @@
 // To parse this JSON data, do
 //
-//     final modelGetUserInfo = modelGetUserInfoFromJson(jsonString);
+//     final getUserInfo = getUserInfoFromJson(jsonString);
 
 import 'dart:convert';
 
-ModelGetUserInfo modelGetUserInfoFromJson(String str) => ModelGetUserInfo.fromJson(json.decode(str));
+GetUserInfo getUserInfoFromJson(String str) =>
+    GetUserInfo.fromJson(json.decode(str));
 
-String modelGetUserInfoToJson(ModelGetUserInfo data) => json.encode(data.toJson());
+String getUserInfoToJson(GetUserInfo data) => json.encode(data.toJson());
 
-class ModelGetUserInfo {
-    ModelGetUserInfo({
-        this.success,
-        this.data,
-    });
+class GetUserInfo {
+  GetUserInfo({
+    this.success,
+    this.data,
+  });
 
-    bool success;
-    Data data;
+  bool success;
+  Data data;
 
-    factory ModelGetUserInfo.fromJson(Map<String, dynamic> json) => ModelGetUserInfo(
+  factory GetUserInfo.fromJson(Map<String, dynamic> json) => GetUserInfo(
         success: json["success"],
         data: Data.fromJson(json["data"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "success": success,
         "data": data.toJson(),
-    };
+      };
 }
 
 class Data {
-    Data({
-        this.latitude,
-        this.longitude,
-        this.id,
-        this.name,
-        this.accTime,
-        this.topTime,
-        this.eventLog,
-        this.v,
-    });
+  Data({
+    this.latitude,
+    this.longitude,
+    this.id,
+    this.name,
+    this.accTime,
+    this.topTime,
+    this.eventLog,
+    this.v,
+  });
 
-    int latitude;
-    int longitude;
-    String id;
-    String name;
-    int accTime;
-    int topTime;
-    List<dynamic> eventLog;
-    int v;
+  int latitude;
+  int longitude;
+  String id;
+  String name;
+  int accTime;
+  int topTime;
+  List<dynamic> eventLog;
+  int v;
 
-    factory Data.fromJson(Map<String, dynamic> json) => Data(
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
         latitude: json["latitude"],
         longitude: json["longitude"],
         id: json["_id"],
@@ -58,9 +59,9 @@ class Data {
         topTime: json["topTime"],
         eventLog: List<dynamic>.from(json["eventLog"].map((x) => x)),
         v: json["__v"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "latitude": latitude,
         "longitude": longitude,
         "_id": id,
@@ -69,5 +70,5 @@ class Data {
         "topTime": topTime,
         "eventLog": List<dynamic>.from(eventLog.map((x) => x)),
         "__v": v,
-    };
+      };
 }
