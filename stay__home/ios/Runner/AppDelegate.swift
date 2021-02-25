b2eb09a7292da0e5619be4f9ead0b49bb39c3cb1
Dev-Beom <rtw_master@naver.com>
2021-02-25 20:16:55 +0900

    Fixed IOS Version

--- stay__home/ios/Runner/AppDelegate.swift
+++ stay__home/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import UIKit
 import Flutter
+import workmanager
+import shared_preferences
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
     GeneratedPluginRegistrant.register(with: self)
+    WorkmanagerPlugin.register(with: self.registrar(forPlugin: "be.tramckrijte.workmanager.WorkmanagerPlugin")!)
+    UNUserNotificationCenter.current().delegate = self
+    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*10))
+
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in        
+        FLTSharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+        
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
